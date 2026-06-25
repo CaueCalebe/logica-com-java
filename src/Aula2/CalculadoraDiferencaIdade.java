efead8f6f2d9aab0f4c1af2529ffe8936761825b
CaueCalebe <cauecalebe070@gmail.com>
2026-06-25 19:45:08 -0300

    feat(aula2): adiciona exercício da aula 2

--- src/Aula2/CalculadoraDiferencaIdade.java
+++ src/Aula2/CalculadoraDiferencaIdade.java
@@ -2,5 +2,15 @@ package Aula2;
 
 
 public class CalculadoraDiferencaIdade {
+    public static void main(String[] args){
+        
+    int idade1 = 10;
+    int idade2 = 5;
+    int diferenca;
     
+    diferenca = idade1 - idade2;
+    
+    System.out.println("A diferença das idades é de: " + diferenca + " anos");
+    
+    } 
 }
