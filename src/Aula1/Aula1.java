f0e5f4a8477e113980557aca55c1fb37fa2da820
CaueCalebe <cauecalebe070@gmail.com>
2026-06-25 19:07:13 -0300

    refactor(aula1): atualizando sintaxe do Aula1.java

--- src/Aula1/Aula1.java
+++ src/Aula1/Aula1.java
@@ -1,6 +1,8 @@
 package Aula1;
 
+// A "//" serve para adicionar um comentário curto
 
+/* A "/*" serve para adicionar um comentário longo */
 public class Aula1 {
     public static void main(String[] args){
         int idade = 40;
