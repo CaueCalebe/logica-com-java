2d52ebb161603d49e3cffad1f221f210474a5de4
CaueCalebe <cauecalebe070@gmail.com>
2026-07-02 19:25:49 -0300

    refactor(aula3): atualizar sintaxe CalcularPorcentagemSalario

--- src/Aula3/CalcularPorcentagemSalario.java
+++ src/Aula3/CalcularPorcentagemSalario.java
@@ -1,8 +1,22 @@
 package Aula3;
 
+import java.util.Scanner;
+
 public class CalcularPorcentagemSalario {
-    public static void main(String[] args){
-        
-        
+    //public static void main(String[] args){
+      double resultadoSalario;
+      
+      Scanner teclado = new Scanner(System.in);
+      
+      System.out.println("Escreva o valor do seu Salário: ");
+      double salario = teclado.nextDouble();
+      
+      System.out.println("Escreva o percentual do salário: ");
+      double percentualSalario = teclado.nextDouble();
+      
+      resultadoSalario = salario * (percentualSalario / 100);
+      
+      System.out.println("O Resultado do Percentual do Salário é: R$" + resultadoSalario);
+      
     }
 }
