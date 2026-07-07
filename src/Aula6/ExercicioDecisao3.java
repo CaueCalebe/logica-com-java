b82d8d7a13c41fe64e537863f47ba7049ba6ebe9
CaueCalebe <cauecalebe070@gmail.com>
2026-07-07 13:42:55 -0300

    feat(aula6): adicionar ExercicioDecisao3.java

---
+++ src/Aula6/ExercicioDecisao3.java
@@ -0,0 +1,25 @@
+package Aula6;
+
+import java.util.Scanner;
+
+public class ExercicioDecisao3 {
+    public static void main (String[] args) {
+        int numeroEscolhido;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite um número: ");
+        numeroEscolhido = teclado.nextInt();
+        
+        if (numeroEscolhido % 2 == 0) {
+            
+            System.out.println("" + numeroEscolhido + " é Par!");
+        }
+        else {
+            
+            System.out.println("" + numeroEscolhido + " é Ímpar!");
+        }
+        
+        System.out.println("Fim do Programa!");
+    }
+}
