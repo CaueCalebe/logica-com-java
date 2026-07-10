2c3c782493cb2e37652dd95d5ff60138b5962d96
CaueCalebe <cauecalebe070@gmail.com>
2026-07-10 18:54:38 -0300

    feat(aula11): adicionar DiasDaSemana.java

---
+++ src/Aula11/DiasDaSemana.java
@@ -0,0 +1,44 @@
+package Aula11;
+
+import java.util.Scanner;
+
+public class DiasDaSemana {
+    public static void main (String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite um número de (1) à (7): ");
+        int numeroDia = teclado.nextInt();
+        
+        if (numeroDia == 1) {
+            
+            System.out.println("Dia 1 - Segunda-feira"); 
+        }
+        else if (numeroDia == 2) {
+            
+            System.out.println("Dia 2 - Terça-feira");
+        }
+        else if (numeroDia == 3) {
+            
+            System.out.println("Dia 3 - Quarta-feira");
+        }
+        else if (numeroDia == 4) {
+            
+            System.out.println("Dia 4 - Quinta-feira");
+        }
+        else if (numeroDia == 5) {
+            
+            System.out.println("Dia 5 - Sexta-feira");
+        }
+        else if (numeroDia == 6) {
+            
+            System.out.println("Dia 6 - Sábado");
+        }
+        else {
+            
+            System.out.println("Dia 7 - Domingo");
+        }
+        
+        System.out.println("Fim do Programa!!");
+    }
+}
