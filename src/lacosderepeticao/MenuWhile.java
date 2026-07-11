d667f8026ffd76c779255dd1b07321c86b2981d4
CaueCalebe <cauecalebe070@gmail.com>
2026-07-11 13:06:25 -0300

    feat(lacosderepeticao): adicionar MenuWhile.java

---
+++ src/lacosderepeticao/MenuWhile.java
@@ -0,0 +1,24 @@
+package lacosderepeticao;
+
+import java.util.Scanner;
+
+public class MenuWhile {
+    public static void main(String[] args) {
+         
+        Scanner teclado = new Scanner(System.in);
+        int numeroDigitado = 0;
+        
+        do {
+            
+            System.out.println("---- Menu Navegável ----\n");
+            System.out.println("Digite um Número de (1) à (3): \n");
+            System.out.println("1. Calcular Imposto");
+            System.out.println("2. Depositar Salário");
+            System.out.println("3. Sair");
+            numeroDigitado = teclado.nextInt();
+        }
+        while (numeroDigitado != 3);
+        
+        System.out.println("\nFim do Programa!");
+    }
+}
