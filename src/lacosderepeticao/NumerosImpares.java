4def05e4d8429c999a13bc5bb2d7ef29816c53eb
CaueCalebe <cauecalebe070@gmail.com>
2026-07-11 13:05:48 -0300

    feat(lacosderepeticao): adicionar NumerosImpares.java

---
+++ src/lacosderepeticao/NumerosImpares.java
@@ -0,0 +1,24 @@
+package lacosderepeticao;
+
+import java.util.Scanner;
+
+public class NumerosImpares {
+    public static void main(String[] args) {
+        int contador = 1;
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("---- Contagem de Números Ímpares ----\n");
+        
+        System.out.println("Digite um Número para a Contagem: ");
+        int numeroDigitado = teclado.nextInt();
+        
+        while (contador <= numeroDigitado) {
+            
+            System.out.println("Número: " + contador);
+            contador = contador + 2;
+        }
+        
+        System.out.println("\nFim do Programa!");
+    }
+}
