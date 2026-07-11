c5bea759195357883905306e636503b3830d9d27
CaueCalebe <cauecalebe070@gmail.com>
2026-07-11 12:44:18 -0300

    feat(lacosderepeticao): adicionar TelaLogin.java

---
+++ src/lacosderepeticao/TelaLogin.java
@@ -0,0 +1,44 @@
+package lacosderepeticao;
+
+import java.util.Scanner;
+
+public class TelaLogin {
+    public static void main(String[] args) {
+        
+        String login = "Cauê Calebe";
+        String senha = "Calebe0716";
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        do {
+          
+            System.out.println("\n---- Tela de Login ----\n");
+        
+            System.out.println("Login: ");
+            login = teclado.nextLine();
+        
+            System.out.println("\nSenha: ");
+            senha = teclado.nextLine();
+            
+            if (login.equals("Cauê Calebe") && senha.equals("Calebe0716")) {
+                
+                System.out.println("\nACESSO CONCEDIDO!");
+            }
+            else if (login == "" || senha == "") {
+                
+                System.out.println("\nLOGIN NÃO LOCALIZADO!");
+            }
+            else {
+                
+                System.out.println("\nACESSO NEGADO!");
+            }
+            
+        } while (!login.equals("Cauê Calebe") || !senha.equals("Calebe0716"));
+        
+        teclado.close();
+        
+        System.out.println("\nFim do Programa!!");
+        
+    }
+ 
+}
