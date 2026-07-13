894cc921f7456d1ae08c01a66f4d775b080bab82
CaueCalebe <cauecalebe070@gmail.com>
2026-07-13 18:18:03 -0300

    feat(lacosderepeticaofor): adicionar QuadradoNumeroInteiro.java

---
+++ src/lacosderepeticaofor/QuadradoNumeroInteiro.java
@@ -0,0 +1,15 @@
+package lacosderepeticaofor;
+
+public class QuadradoNumeroInteiro {
+    public static void main(String[] args) {
+        
+        System.out.println("---- Tabela Quadrado Perfeito do 15 até 200 ----\n");
+        
+        for (int i = 15; i <= 200; i++) {
+            
+            System.out.println("Quadrado Perfeito de " + i + " é: " + (i * i));
+        }
+        
+        System.out.println("\nFim do Programa!");
+    }
+}
