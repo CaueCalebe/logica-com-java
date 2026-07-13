71dda38e33bb0a0e3026a2e7e52e2bd5b8c178a0
CaueCalebe <cauecalebe070@gmail.com>
2026-07-13 18:16:42 -0300

    feat(lacosderepeticaofor): adicionar IntroducaoFor.java

---
+++ src/lacosderepeticaofor/IntrudocaoFor.java
@@ -0,0 +1,13 @@
+package lacosderepeticaofor;
+
+import java.util.Scanner;
+
+public class IntrudocaoFor {
+    public static void main(String[] args) {
+        
+        for (int contador = 0; contador <= 10; contador++) {
+           
+            System.out.println(contador);
+        }
+    }
+}
