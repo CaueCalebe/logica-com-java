7051ad819568dbcfea42061bdc199a8cb0779830
CaueCalebe <cauecalebe070@gmail.com>
2026-07-14 17:14:11 -0300

    feat(arrays): aicionar IntroducaoArrays.java

---
+++ src/arrays/IntroducaoArrays.java
@@ -0,0 +1,18 @@
+package arrays;
+
+public class IntroducaoArrays {
+    public static void main(String[] args) {
+        
+        double[] notas = new double[4];
+        
+        notas[0] = 7.5;
+        notas[1] = 8.0;
+        notas[2] = 3;
+        notas[3] = 6;
+        
+        System.out.println(notas[0]);
+        System.out.println(notas[1]);
+        System.out.println(notas[2]);
+        System.out.println(notas[3]);
+    }
+}
