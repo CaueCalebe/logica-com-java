1d7f60100b88afae46869a295c4ce46057f7acee
CaueCalebe <cauecalebe070@gmail.com>
2026-07-14 16:39:03 -0300

    refactor(arrays): atualizar sintaxe ArraysComFor.java

--- src/arrays/ArraysComFor.java
+++ src/arrays/ArraysComFor.java
@@ -15,10 +15,15 @@ public class ArraysComFor {
             notas[i] = teclado.nextDouble();
         }
         
+        double media = 0;
+        
         for (int i = 0; i < notas.length; i++) {
-            
+            media = media + notas[i];
             System.out.println("Nota " + (i + 1) + ": " + notas[i]);
         }
+        
+        media = media / notas.length;
+        System.out.println("\nMédia das Notas: " + media);
         
         teclado.close();
         
